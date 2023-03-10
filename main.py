dcf78ccaab7bb49d151005a4aeb03d9457faac81
Koraytopuz <91742967+Koraytopuz@users.noreply.github.com>
2023-03-10 21:42:01 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+r1="kırmızı"
+r2="yeşil"
+if r1=="kırmızı":
+    if r2=="yeşil":
+        print("renklerin karışımı mor")
+    else:
+        print("renklerin karışımı pembe")
+    elif r1=="yeşil":
+    if r2=="kırmızı":
+        print("renklerin karışımı mor")
